7ebd2a694dd22e811c433c42becd47ab3bbe1e27
chowdhurymonojeet <chowdhurymonojeet@gamil.com>
2024-11-04 21:54:47 +0530

    cleaned code

--- main.py
+++ main.py
@@ -24,12 +24,6 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
-
-
-
-
-
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -52,9 +46,7 @@ def main():
 
         dt = clock.tick(60) / 1000
     
-    # print("Starting asteroids!")
-    # print(f"Screen width : {SCREEN_WIDTH}")
-    # print(f"Screen height : {SCREEN_HEIGHT}")
+    
 
 if __name__ == "__main__":
     main()
